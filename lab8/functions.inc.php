c0480430ccf8a4192baab401dae0b312f977f702
yiqingqiu23187 <18302010034@fudan.edu.cn>
2019-05-21 14:47:31 +0800

    lab8提交

--- lab8/functions.inc.php
+++ lab8/functions.inc.php
@@ -1,8 +1,20 @@
 <?php
 
-    function outputOrderRow($file, $title, $quantity, $price) {
-        echo "<tr>";
-        //TODO
-        echo "</tr>";
-    }
+function outputOrderRow($file, $title, $quantity, $price)
+{
+    $imgsrc = 'images/books/tinysquare/' . $file;
+    $realprice = "$" . number_format($quantity, 2, ".", "");
+    $realamount = "$" . number_format($quantity * $price, 2, ".", "");
+    echo "<tr>";
+    //TODO
+
+    echo "<td><img src=$imgsrc></td>";
+    echo "<td>$title</td>";
+    echo "<td>$quantity</td>";
+    echo "<td>$realprice</td>";
+    echo "<td>$realamount</td>";
+
+    echo "</tr>";
+}
+
 ?>
